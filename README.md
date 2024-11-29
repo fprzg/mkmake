d0cb6618b29281807e5dd344cfc1dca3fc2cd122
fprzg <faride.perezgarcia@gmail.com>
2024-11-28 19:41:48 -0600

    Basic funcionality for describing the rules added. NAME, PHONY, DESC, REQ and STEP. README.md updated. test rule added to Makefile

--- README.md
+++ README.md
@@ -4,9 +4,40 @@ Small utility for creating Makefile files.
 
 ## Installation
 The script gets installed in **~/.local/bin**. If such directory doesn't exist, it gets created.
-To install, go to the git repository directory and type:
-```bash
-make install
-```
+To install, run `make install` on the root of the directory of the git repository.
 
 ## Usage
+There are a few keywords:
+* `NAME the/rule/name`: Sets the name of the rule. If there's no name, it raises an error. If there's more than one name, also raises an error.
+* `PHONY`: Adds the rule as `.PHONY`.
+* `DESC the actual description`: Sets the description of the rule. Optional.
+* `REQ another/rule`: If you want to execute another rule before the current one, you can add it here. You can add more than one rule, or none.
+* `STEP echo "This is the step"`: You use this to add the steps of the recipe. You can add one, or none.
+
+Default behavior is to append rules to an existing Makefile if there's one if $PWD. If there is not, it creates one. You can output to stdout with `-P` or overwrite the current Makefile with `-O`.
+
+```bash
+mkmake -dP "NAME update DESC Updates the system (super user privileges required) REQ confirm STEP @echo Updating system... STEP @sudo apt -y update && echo System updated"
+```
+
+outputs to stdout the following:
+
+```Makefile
+## help: shows this message.
+.PHONY: help
+help: 
+	@echo 'Usage'
+	@sed -n 's/^##//p' ${MAKEFILE_LIST} | column -t -s ':' | sed -e 's/^/ /'
+
+
+## confirm: asks the user for confirmation.
+.PHONY: confirm
+confirm: 
+	@echo -n 'Are you sure? [y/N] ' && read ans && [ $${ans:-N} = y ]
+
+
+## update: Updates the system (super user privileges required).
+update: confirm
+	@echo Updating system...
+	@sudo apt -y update && echo System updated
+```
---
+++ mkmake.py
@@ -0,0 +1,136 @@
+#! /bin/python3
+
+import argparse
+import re
+
+class MakefileRule:
+	phony = True
+
+	def __init__(self, name, phony=True, description=None, requires=[], recipe=[]):
+		self.name = name
+		self.phony = phony
+		self.description = description
+		self.requires = requires
+		self.recipe = recipe
+
+	def __str__(self) -> str:
+		as_str = ""
+		if self.description != None:
+			as_str += f"## {self.name}: {self.description}\n"
+		if self.phony:
+			as_str += f".PHONY: {self.name}\n"
+		as_str += f"{self.name}: {' '.join(req for req in self.requires)}\n"
+		as_str += ''.join(f"\t{step}\n" for step in self.recipe)
+		return as_str
+
+	@classmethod
+	def from_dict(cls, rule_dict):
+		if rule_dict["NAME"] == None:
+			raise ValueError("Cannot create a MakefileRule without a name")
+
+		return cls(rule_dict["NAME"],
+			phony=rule_dict["PHONY"],
+			description=rule_dict["DESC"],
+			requires=rule_dict["REQ"],
+			recipe=rule_dict["STEP"])
+
+	@classmethod
+	def from_str(cls, rule_str):
+		keywords = [ "PHONY", "NAME", "DESC", "REQ", "STEP"]
+		keywords_str = rf"{ '|'.join(re.escape(k) for k in keywords) }"
+		pattern = re.compile(rf"({keywords_str})\s(.*?)(?=(?:{keywords_str})|$)")
+
+		matches = pattern.findall(rule_str)
+
+		rule_dict = {
+			"PHONY": False,
+			"NAME": "",
+			"DESC": None,
+			"REQ": [],
+			"STEP": [],
+		}
+
+		for match in matches:
+			keyword, content = match
+			content = content.strip()
+
+			match keyword:
+				case "PHONY":
+					rule_dict[keyword] = True
+				case "NAME":
+					if content == "" or content == None:
+						raise ValueError("NAME cannot be empty")
+					rule_dict[keyword] = content
+				case "DESC":
+					rule_dict[keyword] = content
+				case "REQ" | "STEP":
+					rule_dict[keyword].append(content)
+
+		return  cls.from_dict(rule_dict)
+
+	@classmethod
+	def from_params(cls, name, phony=None, description=None, requires=[], recipe=[]):
+		return cls(name,
+			phony=phony if phony != None else cls.phony,
+			description=description,
+			requires=requires,
+			recipe=recipe)
+
+	@classmethod
+	def disable_phony(cls):
+		cls.phony = False
+
+	@classmethod
+	def enable_phony(cls):
+		cls.phony = True
+
+
+class MakefileRuleList:
+	def __init__(self):
+		self.rules = {}
+
+	def add_rule(self, new_rule):
+		self.rules[new_rule.name] = new_rule
+
+	def add_rule_str(self, new_rule_str):
+		new_rule = MakefileRule.from_str(new_rule_str)
+		self.rules[new_rule.name] = new_rule
+
+	def __str__(self) -> str:
+		return '\n\n'.join(str(rule) for name, rule in self.rules.items())
+
+
+def main():
+	parser = argparse.ArgumentParser(
+		prog='mkmake',
+		description='Utility for automated Makefile creation.'
+	)
+
+	parser.add_argument('-p', '--disable-phony', action='store_true', help='prevent rules from being added to .PHONY.')
+	parser.add_argument('-d', '--add-default-rules', action='store_true', help='add \'help\' and \'confirm\'.')
+	parser.add_argument('-P', '--print', action='store_true', help='print to string instead of Makefile.')
+	parser.add_argument('-O', '--overwrite', action='store_true', help='overwrite Makefile.')
+	parser.add_argument('rules', nargs='*', help='list of rules.')
+	args = parser.parse_args()
+
+	rules = MakefileRuleList()
+	if args.disable_phony:
+		MakefileRule.disable_phony()
+
+	if args.add_default_rules:
+		rules.add_rule(MakefileRule.from_params('help', description='shows this message.', recipe=["@echo 'Usage'", "@sed -n 's/^##//p' ${MAKEFILE_LIST} | column -t -s ':' | sed -e 's/^/ /'" ]))
+		rules.add_rule(MakefileRule.from_params('confirm', description='asks the user for confirmation.', recipe=["@echo -n 'Are you sure? [y/N] ' && read ans && [ $${ans:-N} = y ]"]))
+
+	for rule in args.rules:
+		rules.add_rule_str(rule)
+
+	if args.print:
+		print(rules)
+	else:
+		mode = 'a' if not args.overwrite else 'w'
+		with open('Makefile', mode) as f:
+			print(rules, file=f)
+
+
+if __name__ == "__main__":
+	main()
